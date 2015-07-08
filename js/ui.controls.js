d68050ae79c0e294706977d5387692ac8b846011
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-07-08 14:35:02 +0200

    initial hack

---
+++ js/ui.controls.js
@@ -0,0 +1,266 @@
+/**
+ui.control.js
+Created by Pitch Interactive
+Created on 6/26/2012
+This code will control the primary functions of the UI in the ArmsGlobe app
+**/
+d3.selection.prototype.moveToFront = function() { 
+    return this.each(function() { 
+        this.parentNode.appendChild(this); 
+    }); 
+}; 
+
+var d3Graphs = {
+    barGraphWidth: 300,
+	barGraphHeight: 800,
+    barWidth: 14,
+	barGraphTopPadding: 20,
+	barGraphBottomPadding: 50,
+	histogramWidth: 686,
+	histogramHeight: 160,
+	histogramLeftPadding:31,
+	histogramRightPadding: 31,
+	histogramVertPadding:20,
+	barGraphSVG: d3.select("#wrapper").append("svg").attr('id','barGraph'),
+	histogramSVG: null,
+	histogramYScale: null,
+	histogramXScale: null,
+	cumImportY: 0,cumExportY: 0,
+    cumImportLblY: 0,cumExportLblY: 0,
+    inited: false,
+    histogramOpen: false,
+    handleLeftOffset: 12,
+    handleInterval: 35,
+    windowResizeTimeout: -1,
+    histogramImports: null,
+    histogramExports: null,
+    histogramAbsMax: 0,
+    previousImportLabelTranslateY: -1,
+    previousExportLabelTranslateY: -1,
+    zoomBtnInterval: -1,
+
+
+    setCountry: function(country) {
+        d3Graphs.updateViz();
+    },
+    zoomBtnMouseup: function() {
+        clearInterval(d3Graphs.zoomBtnInterval);
+    },
+    zoomBtnClick:function() {
+        var delta;
+        if($(this).hasClass('zoomOutBtn')) {
+            delta = -0.5;
+        } else {
+            delta = 0.5;
+        }
+        d3Graphs.doZoom(delta);
+        d3Graphs.zoomBtnInterval = setInterval(d3Graphs.doZoom,50,delta);
+    },
+    doZoom:function(delta) {
+        camera.scale.z += delta * 0.1;
+        camera.scale.z = constrain( camera.scale.z, 0.8, 5.0 );
+    },
+    toggleAboutBox:function() {
+        $("#aboutContainer").toggle();
+    },
+    clickTimeline:function() {
+        var year = $(this).html();
+        if(year < 10) {
+            year = (year * 1) + 2000;
+        }
+        if(year < 100) {
+            year = (year * 1) + 1900
+        }
+        var index = year - 1992;
+        var leftPos = d3Graphs.handleLeftOffset + d3Graphs.handleInterval * index;
+        $("#handle").css('left',leftPos+"px");
+        d3Graphs.updateViz();
+    },
+    windowResizeCB:function() {
+        clearTimeout(d3Graphs.windowResizeTimeout);
+        d3Graphs.windowResizeTimeout = setTimeout(d3Graphs.windowResize, 50);
+    },
+    windowResize: function() {
+        var windowWidth = $(window).width();
+        var windowHeight = $(window).height();
+        d3Graphs.positionHistory(windowWidth);
+        var minWidth = 1280;
+        var minHeight = 860;
+        var w = windowWidth < minWidth ? minWidth : windowWidth;
+        var hudButtonWidth = 489;
+        $('#hudButtons').css('left',w - hudButtonWidth-20);        
+        var importExportButtonWidth = $("#importExportBtns").width();
+        $("#importExportBtns").css('left',w-importExportButtonWidth - 20);
+        var barGraphHeight = 800;
+        var barGraphBottomPadding = 10;
+        console.log(windowHeight+ " " + barGraphHeight + " " + barGraphBottomPadding);
+        var barGraphTopPos = (windowHeight < minHeight ? minHeight : windowHeight) - barGraphHeight - barGraphBottomPadding;
+        console.log(barGraphTopPos);
+        
+        $("#barGraph").css('top',barGraphTopPos+'px');
+        /*
+        var hudHeaderLeft = $("#hudHeader").css('left');
+        hudHeaderLeft = hudHeaderLeft.substr(0,hudHeaderLeft.length-2)
+        console.log(hudHeaderLeft);
+        var hudPaddingRight = 30;
+        $("#hudHeader").width(w-hudHeaderLeft - hudPaddingRight);
+        */
+    },
+    positionHistory: function(windowWidth) {
+        var graphIconPadding = 20;
+        var historyWidth = $("#history").width();
+        var totalWidth = historyWidth + $("#graphIcon").width() + graphIconPadding;
+//        var windowWidth = $(window).width();
+        var historyLeftPos = (windowWidth - totalWidth) / 2.0;
+        var minLeftPos = 280;
+        if(historyLeftPos < minLeftPos) {
+            historyLeftPos = minLeftPos;
+        }
+        $("#history").css('left',historyLeftPos+"px");
+        $("#graphIcon").css('left',historyLeftPos + historyWidth + graphIconPadding+'px');
+    },
+    countryFocus:function(event) {
+        //console.log("focus");
+        setTimeout(function() { $('#hudButtons .countryTextInput').select() },50);
+    },
+    menuItemClick:function(event) {
+        d3Graphs.updateViz();
+    },
+    countryKeyUp: function(event) {
+        if(event.keyCode == 13 /*ENTER */) {
+            d3Graphs.updateViz();
+        }
+    },
+    
+    updateViz:function() {
+        var yearOffset = $("#handle").css('left');
+        yearOffset = yearOffset.substr(0,yearOffset.length-2);
+        yearOffset -= d3Graphs.handleLeftOffset;
+        yearOffset /= d3Graphs.handleInterval;
+        var year = yearOffset + 1992;
+        
+        var country = $("#hudButtons .countryTextInput").val().toUpperCase();
+        if(typeof countryData[country] == 'undefined') {
+            return;
+        }
+        
+        //exports first
+        var exportArray = []
+        var exportBtns = $("#importExportBtns .exports>div").not(".label");
+        for(var i = 0; i < exportBtns.length; i++) {
+            var btn = $(exportBtns[i]);
+            var weaponTypeKey = btn.attr('class');
+            var weaponName = reverseWeaponLookup[weaponTypeKey];
+
+            if(btn.find('.inactive').length == 0) {
+                exportArray.push(weaponName);
+                selectionData.exportCategories[weaponName] = true;
+            } else {
+                selectionData.exportCategories[weaponName] = false;
+            }
+        }
+        //imports esecond
+        var importArray = []
+        var importBtns = $("#importExportBtns .imports>div").not(".label");
+        for(var i = 0; i < importBtns.length; i++) {
+            var btn = $(importBtns[i]);
+            var weaponTypeKey = btn.attr('class');
+            var weaponName = reverseWeaponLookup[weaponTypeKey];
+            if(btn.find('.inactive').length == 0) {
+                importArray.push(weaponName);
+                selectionData.importCategories[weaponName] = true;
+            } else {
+                selectionData.importCategories[weaponName] = false;
+            }
+        }
+        selectVisualization(moves);
+    },
+    dropHandle:function() {
+        d3Graphs.updateViz();
+    },
+    importExportLabelClick: function() {
+        var btns = $(this).prevAll();
+        var numInactive = 0;
+        for(var i = 0; i < btns.length; i++) {
+            if($(btns[i]).find('.inactive').length > 0) {
+                numInactive++;
+            }
+        }
+        if(numInactive <= 1) {
+            //add inactive
+            $(btns).find('.check').addClass('inactive');
+        } else {
+            //remove inactive
+            $(btns).find('.check').removeClass('inactive');
+        }
+        d3Graphs.updateViz();
+    },
+    importExportBtnClick:function() { 
+        var check = $(this).find('.check');
+        if(check.hasClass('inactive')) {
+            check.removeClass('inactive');
+        } else {
+            check.addClass('inactive');
+        }
+        d3Graphs.updateViz();
+    },
+    graphIconClick: function() {
+        if(!d3Graphs.histogramOpen) {
+            d3Graphs.histogramOpen = true;
+            $("#history .graph").slideDown();
+        } else {
+            d3Graphs.closeHistogram();
+        }
+    },
+    closeHistogram: function() {
+        d3Graphs.histogramOpen = false;
+        $("#history .graph").slideUp();
+    },
+    line: d3.svg.line()
+        // assign the X function to plot our line as we wish
+    .x(function(d,i) { 
+        if(d == null) {
+            return null;
+        }
+        return d3Graphs.histogramXScale(d.x) + d3Graphs.histogramLeftPadding; 
+     })
+    .y(function(d) { 
+        if(d == null) {
+            return null;
+        }
+        return d3Graphs.histogramYScale(d.y) + d3Graphs.histogramVertPadding; 
+    }),
+    dragHandleStart: function(event) {
+        console.log('start');
+        event.dataTransfer.setData('text/uri-list','yearHandle.png');
+        event.dataTransfer.setDragImage(document.getElementById('handle'),0,0);
+        event.dataTransfer.effectAllowed='move';
+    }
+}
+
+/*
+This is going to be a number formatter. Example of use:
+
+var bigNumber = 57028715;
+var formated = abbreviateNumber(57028715);
+return formated; //should show 57B for 57 Billion
+
+*/
+function abbreviateNumber(value) {
+    
+    var newValue = value;
+    if (value >= 1000) {
+        var suffixes = ["", "K", "M", "B","T"];
+        var suffixNum = Math.floor( (""+value).length/3 );
+        var shortValue = '';
+        for (var precision = 3; precision >= 1; precision--) {
+            shortValue = parseFloat( (suffixNum != 0 ? (value / Math.pow(1000,suffixNum) ) : value).toPrecision(precision));
+            var dotLessShortValue = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g,'');
+            if (dotLessShortValue.length <= 3) { break; }
+        }
+        if (shortValue % 1 != 0)  shortNum = shortValue.toFixed(1);
+        newValue = shortValue+suffixes[suffixNum];
+    }
+    return '$' + newValue;
+}
+
---
+++ js/visualize.js
@@ -0,0 +1,271 @@
+function buildDataVizGeometries(bin) {
+
+  var loadLayer = document.getElementById('loading');
+
+  var count = 0;
+  console.log('Building data');
+  for (var s in bin) {
+    var set = bin[s];
+
+    var exporterName = set.e.toUpperCase();
+    var importerName = set.i.toUpperCase();
+
+    exporter = countryData[exporterName];
+    importer = countryData[importerName];
+
+    //	we couldn't find the country, it wasn't in our list...
+    if (exporter === undefined || importer === undefined)
+      continue;
+
+    //	visualize this event
+    set.lineGeometry = makeConnectionLineGeometry(exporter, importer, set.v, set.wc);
+
+    // if( s % 1000 == 0 )
+    // 	console.log( 'calculating ' + s + ' of ' + yearBin.length + ' in year ' + year);
+  }
+
+  //	use this break to only visualize one year (1992)
+  // break;
+
+  //	how to make this work?
+  // loadLayer.innerHTML = 'loading data for ' + year + '...';
+  // console.log(loadLayer.innerHTML);
+
+  loadLayer.style.display = 'none';
+}
+
+function getVisualizedMesh( bin){
+
+	var affectedCountries = [];
+
+	var linesGeo = new THREE.Geometry();
+	var lineColors = [];
+
+	var particlesGeo = new THREE.Geometry();
+	var particleColors = [];			
+
+	// var careAboutExports = ( action === 'exports' );
+	// var careAboutImports = ( action === 'imports' );
+	// var careAboutBoth = ( action === 'both' );
+
+	//	go through the data from year, and find all relevant geometries
+	for( i in bin ){
+		var set = bin[i];
+
+		//	filter out countries we don't care about
+		var exporterName = set.e.toUpperCase();
+		var importerName = set.i.toUpperCase();
+		var relevantExport = true;
+		var relevantImport = true;
+
+		var useExporter = relevantExport;
+		var useImporter = relevantImport;
+
+		var relevantExportCategory = relevantExport;		
+		var relevantImportCategory = relevantImport;		
+
+		if( (useImporter || useExporter) && (relevantExportCategory || relevantImportCategory) ){
+			//	we may not have line geometry... (?)
+			if( set.lineGeometry === undefined )
+				continue;
+
+			var thisLineIsExport = set.white;
+
+			var lineColor = thisLineIsExport ? new THREE.Color(exportColor) : new THREE.Color(importColor);
+
+			var lastColor;
+			//	grab the colors from the vertices
+			for( s in set.lineGeometry.vertices ){
+				var v = set.lineGeometry.vertices[s];		
+				lineColors.push(lineColor);
+				lastColor = lineColor;
+			}
+
+			//	merge it all together
+			THREE.GeometryUtils.merge( linesGeo, set.lineGeometry );
+
+			var particleColor = lastColor.clone();		
+			var points = set.lineGeometry.vertices;
+			// var particleCount = Math.floor(set.v / 8000 / set.lineGeometry.vertices.length) + 1;
+			// particleCount = constrain(particleCount,1,100);
+      particleCount = 5;
+			var particleSize = set.lineGeometry.size;			
+			var particleSize = 15;			
+			for( var s=0; s<particleCount; s++ ){
+				// var rIndex = Math.floor( Math.random() * points.length );
+				// var rIndex = Math.min(s,points.length-1);
+
+				var desiredIndex = s / particleCount * points.length;
+				var rIndex = constrain(Math.floor(desiredIndex),0,points.length-1);
+
+				var point = points[rIndex];						
+				var particle = point.clone();
+				particle.moveIndex = rIndex;
+				particle.nextIndex = rIndex+1;
+				if(particle.nextIndex >= points.length )
+					particle.nextIndex = 0;
+				particle.lerpN = 0;
+				particle.path = points;
+				particlesGeo.vertices.push( particle );	
+				particle.size = particleSize;
+				particleColors.push( particleColor );						
+			}			
+
+			if( $.inArray( exporterName, affectedCountries ) < 0 ){
+				affectedCountries.push(exporterName);
+			}							
+
+			if( $.inArray( importerName, affectedCountries ) < 0 ){
+				affectedCountries.push(importerName);
+			}
+
+			var vb = set.v;
+			var exporterCountry = countryData[exporterName];
+			if( exporterCountry.mapColor === undefined ){
+				exporterCountry.mapColor = vb;
+			}
+			else{				
+				exporterCountry.mapColor += vb;
+			}			
+
+			var importerCountry = countryData[importerName];
+			if( importerCountry.mapColor === undefined ){
+				importerCountry.mapColor = vb;
+			}
+			else{				
+				importerCountry.mapColor += vb;
+			}	
+
+			exporterCountry.exportedAmount += vb;
+			importerCountry.importedAmount += vb;
+
+		}		
+	}
+
+	// console.log(selectedCountry);
+
+	linesGeo.colors = lineColors;	
+
+	//	make a final mesh out of this composite
+	var splineOutline = new THREE.Line( linesGeo, new THREE.LineBasicMaterial( 
+		{ 	color: 0xffffff, opacity: 1.0, blending: 
+			THREE.AdditiveBlending, transparent:true, 
+			depthWrite: false, vertexColors: true, 
+			linewidth: 1 } ) 
+	);
+
+	splineOutline.renderDepth = false;
+
+
+	attributes = {
+		size: {	type: 'f', value: [] },
+		customColor: { type: 'c', value: [] }
+	};
+
+	uniforms = {
+		amplitude: { type: "f", value: 1.0 },
+		color:     { type: "c", value: new THREE.Color( 0xffffff ) },
+		texture:   { type: "t", value: 0, texture: THREE.ImageUtils.loadTexture( "images/particleA.png" ) },
+	};
+
+	var shaderMaterial = new THREE.ShaderMaterial( {
+
+		uniforms: 		uniforms,
+		attributes:     attributes,
+		vertexShader:   document.getElementById( 'vertexshader' ).textContent,
+		fragmentShader: document.getElementById( 'fragmentshader' ).textContent,
+
+		blending: 		THREE.AdditiveBlending,
+		depthTest: 		true,
+		depthWrite: 	false,
+		transparent:	true,
+		// sizeAttenuation: true,
+	});
+
+
+
+	var particleGraphic = THREE.ImageUtils.loadTexture("images/map_mask.png");
+	var particleMat = new THREE.ParticleBasicMaterial( { map: particleGraphic, color: 0xffffff, size: 60, 
+														blending: THREE.NormalBlending, transparent:true, 
+														depthWrite: false, vertexColors: true,
+														sizeAttenuation: true } );
+	particlesGeo.colors = particleColors;
+	var pSystem = new THREE.ParticleSystem( particlesGeo, shaderMaterial );
+	pSystem.dynamic = true;
+	splineOutline.add( pSystem );
+
+	var vertices = pSystem.geometry.vertices;
+	var values_size = attributes.size.value;
+	var values_color = attributes.customColor.value;
+
+	for( var v = 0; v < vertices.length; v++ ) {		
+		values_size[ v ] = pSystem.geometry.vertices[v].size;
+		values_color[ v ] = particleColors[v];
+	}
+
+	pSystem.update = function(){	
+		// var time = Date.now()									
+		for( var i in this.geometry.vertices ){						
+			var particle = this.geometry.vertices[i];
+			var path = particle.path;
+			var moveLength = path.length;
+			
+			particle.lerpN += 0.7;
+			if(particle.lerpN > 1){
+				particle.lerpN = 0;
+				particle.moveIndex = particle.nextIndex;
+				particle.nextIndex++;
+				if( particle.nextIndex >= path.length ){
+					particle.moveIndex = 0;
+					particle.nextIndex = 1;
+				}
+			}
+
+			var currentPoint = path[particle.moveIndex];
+			var nextPoint = path[particle.nextIndex];
+			
+
+			particle.copy( currentPoint );
+			particle.lerpSelf( nextPoint, particle.lerpN );			
+		}
+		this.geometry.verticesNeedUpdate = true;
+	};		
+
+	//	return this info as part of the mesh package, we'll use this in selectvisualization
+	splineOutline.affectedCountries = affectedCountries;
+
+
+	return splineOutline;	
+}
+
+function selectVisualization(){
+	//	clear off the country's internally held color data we used from last highlight
+	for( var i in countryData ){
+		var country = countryData[i];
+		country.exportedAmount = 0;
+		country.importedAmount = 0;
+		country.mapColor = 0;
+	}
+
+	//	clear children
+	while( visualizationMesh.children.length > 0 ){
+		var c = visualizationMesh.children[0];
+		visualizationMesh.remove(c);
+	}
+
+	//	build the mesh
+	console.time('getVisualizedMesh');
+	var mesh = getVisualizedMesh( moves);				
+	console.timeEnd('getVisualizedMesh');
+
+	//	add it to scene graph
+	visualizationMesh.add( mesh );	
+
+	for( var i in mesh.affectedCountries ){
+		var countryName = mesh.affectedCountries[i];
+		var country = countryData[countryName];
+	}
+
+	// console.log( mesh.affectedCountries );
+	highlightCountry( mesh.affectedCountries );
+}
